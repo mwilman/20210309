492b8a835d904afe94027bfe6def7c35c20b1a9c
Maxim Wilman <mwilman@brockhaus-ag.de>
2021-03-09 14:47:57 +0100

    Route Between Feature Libs

--- libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { ReportLossComponent } from './report-loss.component';
-import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
-import { FlightLibModule } from '@flight-workspace/flight-lib';
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule, LuggageFeatureCheckinModule, FlightLibModule],
+  imports: [
+    CommonModule,
+    LuggageDomainModule,
+
+    RouterModule.forChild([
+      { path: '', component: ReportLossComponent }
+    ])
+  ],
   declarations: [ReportLossComponent],
-  exports: [ReportLossComponent],
+  exports: [ReportLossComponent]
 })
-export class LuggageFeatureReportLossModule {}
+export class LuggageFeatureReportLossModule {
+}
