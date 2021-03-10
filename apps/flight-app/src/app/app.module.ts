45992f0d323e1d5b20ecd03f05f1c3265972be99
Maxim Wilman <mwilman@brockhaus-ag.de>
2021-03-10 15:38:17 +0100

    import effects

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from './shared/shared.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './+state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -27,6 +31,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     FlightLibModule.forRoot(),
     SharedModule.forRoot(),
     RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   declarations: [
     AppComponent,
--- apps/luggage/src/app/app.module.ts
+++ apps/luggage/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
 import { HttpClientModule } from '@angular/common/http';
 import { LuggageFeatureReportLossModule } from '@flight-workspace/luggage/feature-report-loss';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule, LuggageFeatureReportLossModule],
+  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule, LuggageFeatureReportLossModule, EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
