e0941b05f2ae1ef0bbd9702650ca47aa501c4c8b
Maxim Wilman <mwilman@brockhaus-ag.de>
2021-03-10 15:42:45 +0100

    adding state

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
@@ -1,15 +1,20 @@
-import { Action, createReducer, on } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import * as FlightBookingActions from './flight-booking.actions';
+import { Flight } from '@flight-workspace/flight-lib';
 
 export const flightBookingFeatureKey = 'flightBooking';
 
 export interface State {
-
+  flights: Flight[];
 }
 
 export const initialState: State = {
+  flights: []
+}
 
-};
+export interface FlightBookingAppState {
+  flightBooking: State
+}
 
 
 export const reducer = createReducer(
